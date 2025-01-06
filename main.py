2c8512455aa5813675aede0c0785fc91cd79ad2c
MinSeokKim <kimjunseok23@gmail.com>
2025-01-07 00:08:40 +0900

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from mytoken import Mytoken
+
+# Intents 설정
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True  # 메시지 콘텐츠 접근 권한 활성화
+
+# Bot 초기화
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Login bot: {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hi!')
+
+bot.run(Mytoken)
