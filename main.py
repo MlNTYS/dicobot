25b932552cd2a20193fc36fa99f846fbb0ae7820
MinSeokKim <kimjunseok23@gmail.com>
2025-01-08 16:40:32 +0900

    Add SQLite3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
+import sqlite3
 from discord.ext import commands
 from mytoken import Mytoken
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
 
 # Intents 설정
 intents = discord.Intents.default()
