77b9957a927abf5fba705f72d963dc4911e43e81
MinSeokKim <kimjunseok23@gmail.com>
2025-01-08 18:03:10 +0900

    경고 명령어 추가

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ cursor.execute('''
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True  # 메시지 콘텐츠 접근 권한 활성화
+intents.members = True  # 서버 멤버 정보 접근 권한 활성화
 
 # Bot 초기화
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -29,8 +30,27 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Login bot: {bot.user}')
 
+
 @bot.command()
-async def hello(ctx):
-    await ctx.send('Hi!')
+async def 경고(ctx, uid: int = None):
+    # UID를 입력하지 않은 경우
+    if uid is None:
+        await ctx.send("사용법: `!경고 <UID>`\nUID를 입력하지 않으셨습니다.")
+        return
+
+    # 서버에서 UID로 멤버 찾기
+    member = ctx.guild.get_member(uid)
+
+    if member:  # 멤버가 존재하면
+        embed = discord.Embed(title="유저 정보", color=discord.Color.blue())
+        embed.add_field(name="닉네임", value=member.display_name, inline=True)
+        embed.add_field(name="ID", value=member.id, inline=True)
+        embed.add_field(name="역할", value=", ".join([role.name for role in member.roles if role.name != "@everyone"]),
+                        inline=False)
+        embed.set_thumbnail(url=member.avatar.url if member.avatar else member.default_avatar.url)
+
+        await ctx.send(embed=embed)
+    else:  # 멤버가 없을 경우
+        await ctx.send(f"ID {uid}에 해당하는 유저를 찾을 수 없습니다.")
 
 bot.run(Mytoken)
